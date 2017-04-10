e088513b7a5404fc2298a1630a59b867fab3f37c
hemmerling <hemmerling@gmx.net>
2017-04-10 11:59:20 +0200

    First release

---
+++ src/hemmerling/com/myfirstci/Main.java
@@ -0,0 +1,17 @@
+package hemmerling.com.myfirstci;
+
+public class Main {
+
+	public Main() {
+	}
+	
+	public int doubleValue(int value) {
+		return(2*value);
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Main MyMain = new Main();
+		System.out.println(MyMain.doubleValue(2));
+	}
+}
