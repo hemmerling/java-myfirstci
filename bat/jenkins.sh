4afed2ab70f3602ce30dc44d580daa7bf72a9391
hemmerling <hemmerling@gmx.net>
2017-05-04 12:57:51 +0200

    Update

--- bat/jenkins.sh
+++ bat/jenkins.sh
@@ -7,9 +7,11 @@ jenkinsUrl="http://localhost:8080"     #URL to trigger Jenkins
 user="admin"
 password="admin"
 trigger="BUILD"
+
 #
 wget_cmd=wget	                       #Location of wget command
 wget_outputToStdout="-O-"
+wget_outputDocument="--output-document -" # must be followed by SPACE and then the URL in single hyphons ( 'http://...' )
 wget_post="--post-data="
 wget_credentials="--auth-no-challenge --http-user=$user --http-password=$password"
 
@@ -23,24 +25,12 @@ curl_credentials="--user $user:$password"
 #$curl_cmd $curl_credentials $curl_post $curl_verbose $jenkinsUrl/job/$jenkinsJob/build?token=$trigger
 
 # Works also with activated CSRF: 
-crumb=$(curl $curl_credentials -s 'http://localhost:8080/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)')
+crumb=$($curl_cmd $curl_credentials -s $jenkinsUrl'/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)')
+#$curl_cmd $curl_credentials $curl_post $curl_verbose -H "$crumb" $jenkinsUrl/job/$jenkinsJob/build?token=$trigger
 $curl_cmd $curl_credentials $curl_post $curl_verbose -H "$crumb" $jenkinsUrl/job/$jenkinsJob/build?token=$trigger
 
+# Works also with activated CSRF: 
+#crumb=$($wget_cmd $wget_credentials $wget_outputDocument $jenkinsUrl'/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)')
+#wget_header=--header="$crumb"
+#$wget_cmd $wget_credentials $wget_post $wget_outputToStdout $wget_header $jenkinsUrl/job/$jenkinsJob/build?token=$trigger
 
-
-#wget --user admin:admin --output-document - \
-#'http://localhost:8080/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)'
-
-
-#curl --user admin:admin -XPOST --show-error http://localhost:8080/job/myfirstci/build?token=BUILD
-
-
-#SERVER=http://localhost:8080
-#CRUMB=$(curl --user $USER:$APITOKEN \
-#    $SERVER/crumbIssuer/api/xml?xpath=concat\(//crumbRequestField,%22:%22,//crumb\))
-#echo $CRUMB
-#user = admin
-#apitoken = admin
-#curl --user $USER:$APITOKEN -XPOST --show-error -H "$CRUMB" http://localhost:8080/job/myfirstci/build?token=BUILD
-#
-#http://localhost:8080/crumbIssuer/api/xml?xpath=concat\(//crumbRequestField,%22:%22,//crumb\))
